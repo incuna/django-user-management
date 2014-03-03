5b72d4df5b853240f61bea3bfd22f03b4a8440db
Max Peterson <max@incuna.com>
2014-03-03 12:37:13 +0000

    Update README and doc string.
    [ci skip]

--- user_management/api/views.py
+++ user_management/api/views.py
@@ -143,6 +143,7 @@ class ProfileAvatar(generics.RetrieveUpdateAPIView):
         height: Specify the height (in pixels) to resize / crop to.
         crop: Whether to crop or not [1,0]
         anchor: Where to anchor the crop [t,r,b,l]
+        upscale: Whether to upscale or not [1,0]
 
     If no options are specified the users avatar is returned.
 
@@ -170,6 +171,22 @@ class UserDetail(generics.RetrieveUpdateDestroyAPIView):
 
 
 class UserAvatar(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve and update the user's avatar. Pass get parameters to
+    retrieve a thumbnail of the avatar.
+
+    Thumbnail options are specified as get parameters. Options are:
+        width: Specify the width (in pixels) to resize / crop to.
+        height: Specify the height (in pixels) to resize / crop to.
+        crop: Whether to crop or not [1,0]
+        anchor: Where to anchor the crop [t,r,b,l]
+        upscale: Whether to upscale or not [1,0]
+
+    If no options are specified the users avatar is returned.
+
+    To crop avatar to 100x100 anchored to the top right:
+        avatar?width=100&height=100&crop=1&anchor=tr
+    """
     model = User
     permission_classes = (IsAuthenticated, permissions.IsAdminOrReadOnly)
     parser_classes = (parsers.MultiPartParser,)
