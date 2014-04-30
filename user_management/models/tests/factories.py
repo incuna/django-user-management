ee2cb8687f6e4ecddfef797cc63c7daa40731dc8
Adam Thomas <sortoflikechess@gmail.com>
2014-04-30 09:44:58 +0100

    Use PostGenerationMethodCall instead of _prepare.

--- user_management/models/tests/factories.py
+++ user_management/models/tests/factories.py
@@ -7,13 +7,4 @@ class UserFactory(factory.DjangoModelFactory):
     FACTORY_FOR = get_user_model()
     name = factory.Sequence(lambda i: 'Test User {}'.format(i))
     email = factory.Sequence(lambda i: 'email{}@example.com'.format(i))
-
-    @classmethod
-    def _prepare(cls, create, **kwargs):
-        password = kwargs.pop('password', None)
-        user = super(UserFactory, cls)._prepare(create=False, **kwargs)
-        user.set_password(password)
-        user.raw_password = password
-        if create:
-            user.save()
-        return user
+    password = factory.PostGenerationMethodCall('set_password', None)
