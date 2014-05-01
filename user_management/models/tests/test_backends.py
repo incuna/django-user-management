5acfa67b88e7c1b0a5724e5db43e3285fc743442
Ian Foote <ian@incuna.com>
2014-05-01 11:39:39 +0100

    Add missing tests for CaseInsensitiveEmailBackend

--- user_management/models/tests/test_backends.py
+++ user_management/models/tests/test_backends.py
@@ -16,3 +16,24 @@ class CaseInsensitveEmailBackendTest(TestCase):
             username='Test-email@example.com', password=password)
 
         self.assertEqual(user, authenticated_user)
+
+    def test_authenticate_no_username(self):
+        email = 'test-Email@example.com'
+        password = 'arandomsuperstrongpassword'
+
+        user = UserFactory.create(email=email, password=password, is_active=True)
+
+        backend = CaseInsensitiveEmailBackend()
+        authenticated_user = backend.authenticate(
+            email='Test-email@example.com', password=password)
+
+        self.assertEqual(user, authenticated_user)
+
+    def test_authenticate_no_user(self):
+        email = 'test-Email@example.com'
+        password = 'arandomsuperstrongpassword'
+
+        backend = CaseInsensitiveEmailBackend()
+        authenticated_user = backend.authenticate(email=email, password=password)
+
+        self.assertIs(authenticated_user, None)
