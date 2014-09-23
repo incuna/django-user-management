2f73bf839b92adb126ea1fd5202d24a0313e6e80
Ian Foote <ian@incuna.com>
2014-09-23 14:57:06 +0100

    Refactor APIRequestTestCase
    * Use incuna-test-utils BaseAPIRequestTestCase

--- user_management/models/tests/utils.py
+++ user_management/models/tests/utils.py
@@ -1,28 +1,11 @@
-from django.contrib.auth.models import AnonymousUser
-from django.test import TestCase
+from incuna_test_utils.testcases.api_request import BaseAPIRequestTestCase
 from incuna_test_utils.testcases.request import BaseRequestTestCase
-from rest_framework.test import APIRequestFactory, force_authenticate
 
 from .factories import UserFactory
 
 
-class APIRequestTestCase(TestCase):
+class APIRequestTestCase(BaseAPIRequestTestCase):
     user_factory = UserFactory
-
-    def create_request(self, method='get', url='/', user=None, auth=True, **kwargs):
-        if not user:
-            if auth:
-                user = self.user_factory.create()
-            else:
-                user = AnonymousUser()
-        kwargs['format'] = 'json'
-        request = getattr(APIRequestFactory(), method)(url, **kwargs)
-        request.user = user
-        if auth:
-            force_authenticate(request, user)
-        if 'data' in kwargs:
-            request.DATA = kwargs['data']
-        return request
 
 
 class RequestTestCase(BaseRequestTestCase):
