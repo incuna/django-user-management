a336022537519b04996300bdf81512525b2d1b3c
Matt Lenc <mattl@incuna.com>
2014-10-31 15:20:30 +0000

    Refactor test with too many requests
    Make less requests and save few pandas ;-)

--- user_management/api/tests/test_views.py
+++ user_management/api/tests/test_views.py
@@ -231,37 +231,24 @@ class TestPasswordResetEmail(APIRequestTestCase):
 
         send_email.assert_called_once_with(user)
 
-    def assert_post_returns_status(self, view, data, expected_status):
-        request = self.create_request('post', data=data, auth=False)
-        response = view(request)
-        self.assertEqual(response.status_code, expected_status)
-
+    @patch('rest_framework.throttling.ScopedRateThrottle.THROTTLE_RATES', new={
+        'passwords': '1/minute',
+    })
     def test_post_rate_limit(self):
-        """
-        Ensure the post requests are rate limited.
-
-        The PasswordResetRateThrottle sets a limit of 3/hour requests.
-        """
+        """Ensure the POST requests are rate limited."""
         email = 'exists@example.com'
         UserFactory.create(email=email)
-        view = self.view_class.as_view()
-        rate_limit = 3
 
-        # Test the the first 3 requests aren't limited.
         data = {'email': email}
-        for i in range(rate_limit):
-            self.assert_post_returns_status(
-                view,
-                data,
-                status.HTTP_204_NO_CONTENT,
-            )
+        request = self.create_request('post', data=data, auth=False)
+        view = self.view_class.as_view()
 
-        # Test that the 4th request is throttled.
-        self.assert_post_returns_status(
-            view,
-            data,
-            status.HTTP_429_TOO_MANY_REQUESTS,
-        )
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        # request is throttled
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_429_TOO_MANY_REQUESTS)
 
     def test_authenticated(self):
         request = self.create_request('post', auth=True)
