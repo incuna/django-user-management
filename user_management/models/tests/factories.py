38221a3d8df945981f9595842871b5dae6a68c0f
Charlie Denton <charlie@meshy.co.uk>
2014-11-11 12:05:27 +0000

    Add raw_password to Users in tests

--- user_management/models/tests/factories.py
+++ user_management/models/tests/factories.py
@@ -7,4 +7,11 @@ class UserFactory(factory.DjangoModelFactory):
     FACTORY_FOR = get_user_model()
     name = factory.Sequence(lambda i: 'Test User {}'.format(i))
     email = factory.Sequence(lambda i: 'email{}@example.com'.format(i))
-    password = factory.PostGenerationMethodCall('set_password', None)
+    is_active = True
+
+    @factory.post_generation
+    def password(self, create, extracted='default password', **kwargs):
+        self.raw_password = extracted
+        self.set_password(self.raw_password)
+        if create:
+            self.save()
