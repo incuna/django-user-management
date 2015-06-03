2422517a0a7a050a12c76040be821a2d902485cb
Kevin Etienne <kevin@incuna.com>
2015-06-03 14:03:57 +0100

    Remove legacy assert
    `last_login` sets to now or None is covered in django

--- user_management/api/tests/test_views.py
+++ user_management/api/tests/test_views.py
@@ -61,14 +61,12 @@ class GetAuthTokenTest(APIRequestTestCase):
 
     def test_post_last_login_updates(self):
         """Authenticating updates the user's last_login."""
-        now = timezone.now()
         user = UserFactory.create(
             email=self.username,
             password=self.password,
-            last_login=now,
         )
-        self.assertEqual(user.last_login, now)
 
+        now = timezone.now()
         request = self.create_request('post', auth=False, data=self.data)
         self.view_class.as_view()(request)
         user = User.objects.get(pk=user.pk)
